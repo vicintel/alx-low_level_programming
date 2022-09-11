f95004592408d262128f4a58a1ff76ded73e64c1
vicintel <victordickson1998@gmail.com>
2022-09-11 18:03:36 +0100

    ass

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l = 'z';
+	
+	while (l >= 'a')
+	{
+		putchar(l);
+		l--;
+	}
+	putchar('\n');
+	return (0);
+}
