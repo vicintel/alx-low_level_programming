e440c34b281f12027bdce73bfcccaa2582ffec0d
vicintel <victordickson1998@gmail.com>
2022-09-11 18:04:59 +0100

    ass

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l;
+
+	int i = 48;
+	
+	while  (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	l = 'a';
+	
+	while (l <= 'f')
+	{
+		putchar(l);
+		l++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
+
