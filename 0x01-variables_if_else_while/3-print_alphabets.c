1e52fe7b31f0570b451839e90ccd42664f12ce34
vicintel <victordickson1998@gmail.com>
2022-09-11 17:54:38 +0100

    ass

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char lower, upper;
+
+	lower = 'a';
+	
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+	
+	upper = 'A';
+	
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
