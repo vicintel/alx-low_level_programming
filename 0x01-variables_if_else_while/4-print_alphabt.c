9cc75f0814496e21fe186283dfe3a9539061232a
vicintel <victordickson1998@gmail.com>
2022-09-11 17:59:10 +0100

    ass

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if (alpha != 'e' && alpha != 'q')
+		{
+			putchar(alpha);
+		}
+		alpha++;
+	}
+
+	putchar('\n');
+	return (0);
+}
